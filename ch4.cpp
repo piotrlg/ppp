7bea098f6bbd0cabf79b58749a7ae20346c2302c
piotrlg <piotrlg@gmail.com>
2018-11-25 21:04:48 +0100

    chapter 4

--- ch4.cpp
+++ ch4.cpp
@@ -1,5 +1,12 @@
 #include "std_lib_facilities.h"
 
+/*
+
+Resolutions
+https://github.com/bewuethr/stroustrup-ppp/tree/master/chapter04
+
+*/
+
 
 // str 104
 void f1()
@@ -400,14 +407,206 @@ void ex2()
     
     // median
     sort(temps);
-    cout << "median: " << temps.size()/2 << endl;
+    double median=0;
+    int size=temps.size();
+
+    if(size%2 != 0)
+        median=temps[(size-1)/2];
+    else
+        median=(temps[size/2-1] + temps[size/2])/2;
+    
+    cout << "median: " << median << endl;
+}
+
+//ex. 3
+void ex3()
+{
+    vector<double> dists;
+    double dist=0;
+    double sum=0;
+    double smallest=0;
+    double largest=0;
+    
+    while(cin>>dist)
+    {
+        dists.push_back(dist);
+        sum+=dist;
+    }
+    if (dists.size() == 0) error("no distances, exiting...");
+    
+    //original BS resolution has bug, min value is always 0
+    // this is the fix
+    smallest=dists[0];
+    largest=dists[0];
+        
+    for(auto i=0; i<dists.size(); ++i)
+    {
+        if ( dists[i] < smallest )
+            smallest=dists[i];
+        else if ( dists[i] > largest )
+            largest=dists[i];
+    }
+
+    cout << "sum of the distances: " << sum << endl;
+    cout << "the smallest distance: " << smallest<< endl;
+    cout << "the largest distance: " << largest<< endl;
+    cout << "mean: " << sum/dists.size() <<endl;
+}
+
+/* przykladowe wartosci
+42
+---
+
+50 <y lm=lm hm=mid mid=(mid-lm)/2+lm(25) 
+50/2=25 <n lm=mid mid=(hm-lm)/2+lm hm=hm
+mid=37 <n lm=mid(37) mid=(50-37)/2+37(43) hm=50
+mid=43 <y lm=(37) mid=(mid-lm)/2+lm(40) hm(43) 
+mid=40 <n lm=40 mid=41 hm=43
+mid=41 <n lm=41 mid=42 hm=43
+
+if hm-lm<=2 stop wynik=mid
+
+    
+    23
+    ---
+    mid=50 <y hm=50 mid=25
+    mid=25 <y hm=25 mid=12
+    mid=12 <n lm=12 mid=18 
+    mid=18 <n lm=18 mid=21 18 21 25
+    mid=21 <n lm=21 mid=23 hm=25
+*/
+
+//ex. 4
+void ex4()
+{
+    char answer=' ';
+    int lower_margin=0;
+    int higher_margin=100;
+    bool run=true;
+    int mid_number=50;
+    
+    cout << "Think of a number 1-100. Answer only y/n."<<endl;
+    while(run)
+    {
+        cout << "Is the number s(maller), b(igger) or c(orrect): " 
+             << mid_number << "? ";
+        cin >> answer;
+        
+        if(answer=='s')
+        {
+            higher_margin = mid_number;
+            mid_number = (mid_number-lower_margin)/2 + lower_margin;
+        }
+        else if(answer=='b')
+        {
+            lower_margin = mid_number;
+            mid_number = (higher_margin-lower_margin)/2 + lower_margin;
+        }
+        else if(answer=='c')
+            run=false;
+        else
+            cout<<"Wrong answer! s(maller), b(igger) or c(orrect)" << endl;
+        
+        //safe guess there is only one number in between
+        if(higher_margin-lower_margin<=2)
+            run=false;
+    }
+    cout << "Your number is: "<<mid_number<<endl;
+}
+
+//ex.5
+void ex5()
+{
+    double val1=0;
+    double val2=0;
+    char oper=' ';
+    bool run=true;
+    
+    while(run)
+    {
+        cout << "Enter 2 values and operator (val1 val2 oper): ";
+        if (cin>>val1>>val2>>oper)
+            switch (oper)
+            {
+                case '+' :
+                    cout << "The sum of " << val1 << " and " << val2 
+                        << " is " << val1 + val2 << endl;
+                    break; 
+                case '-':
+                    cout << "The subtraction of " << val1 << " and " << val2 
+                        << " is " << val1 - val2 << endl;
+                    break;
+                case '*':
+                    cout << "The multiplication of " << val1 << " and " << val2 
+                        << " is " << val1 * val2 << endl;
+                    break;
+                case '/':
+                    if (val2!=0) cout << "The division of " << val1 << " and " << val2 
+                        << " is " << val1 / val2 << endl;
+                    else
+                        cout << "Div by 0 prohibitetd!"<<endl;
+                    break;
+                default:
+                    cout << "Unknown operation!" << endl;
+                    run=false;
+                    break;
+            }
+        else
+            run=false;
+        
+    }
+
+}
+
+//ex.6
+void ex6()
+{
+    vector<string> s_values{"zero","one","two","three","four","five",
+                            "six","seven","eight","nine"};
+    int val=0;
+    string s_number;
+    bool bad_input=true; //it is reset in a loop traversing the vector     
+    int conv_value=0;
+    bool run=true;
+    
+    while(run)
+    {
+        cout << "Enter value (0-9): ";
+        cin>> val;
+        if(val>=0 && val<=9)
+            cout << val << " means: "<< s_values[val] << endl;
+        else
+            cout << "out of range" << endl;
+        
+        cout << "Enter spelled-out number (like: one, two, three), q=quit: ";
+        cin >> s_number;
+        for(int i=0; i<s_values.size(); ++i)
+        {
+            if(s_number == s_values[i])
+            {
+                bad_input=false;
+                conv_value=i;
+                
+            }
+            if(s_number=="q")
+                run=false;
+        }
+        
+        if(bad_input==false) 
+            cout << s_number << " is " << conv_value << endl;
+        else
+            cout << "Wrong number" << endl;
+        
+        bad_input=true;
+    }
+    
 }
 
 
 int main()
 try
 {
-    ex2();
+    ex6();
 }
 catch (runtime_error e)
 {
